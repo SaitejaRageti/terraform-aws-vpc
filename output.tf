d244199bae55babcd5afff8d303a5dff22268331
saitejarageti <saitejarageti129@gmail.com>
2025-06-27 20:55:55 +0530

    terraform

--- output.tf
+++ output.tf
@@ -3,9 +3,17 @@
 
 # } used this ouput block for knowing the available zone names
 
-# output "aws_public_subnet" {
-#   value = aws_subnet.public
-# }
+output "aws_public_subnet" {
+  value = aws_subnet.public[*].id
+}
+
+output "aws_private_subnet" {
+  value = aws_subnet.private[*].id
+}
+
+output "aws_database_subnet" {
+  value = aws_subnet.database[*].id
+}
 
 output "vpc_id" {
 value = aws_vpc.main.id
